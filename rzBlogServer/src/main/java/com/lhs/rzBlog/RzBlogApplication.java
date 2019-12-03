0229a3d137e41934733b93e5a51f143fc98dd4e8
UNO998 <shenyiran2015@outlook.com>
2019-12-03 16:02:11 -0500

    Adding Hello Controller for test

--- rzBlogServer/src/main/java/com/lhs/rzBlog/RzBlogApplication.java
+++ rzBlogServer/src/main/java/com/lhs/rzBlog/RzBlogApplication.java
@@ -1,30 +1,16 @@
 package com.lhs.rzBlog;
 
-import java.util.Arrays;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+
 
 @SpringBootApplication
-public class RzBlogApplication implements CommandLineRunner{
+public class RzBlogApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(RzBlogApplication.class, args);
 	}
-	
-	 @Autowired
-	 private ApplicationContext appContext;
 
-	@Override
-    public void run(String... args) throws Exception {
-
-        String[] beans = appContext.getBeanDefinitionNames();
-        Arrays.sort(beans);
-        for (String bean : beans) {
-            System.out.println(bean);
-        }
-    }
 }
